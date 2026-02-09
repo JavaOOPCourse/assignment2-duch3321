625cf8e68da515cce4575e378ab20fd6591676ad
duch3321 <duch3321@gmail.com>
2026-02-09 20:45:19 +0600

    done

--- src/Library.java
+++ src/Library.java
@@ -10,26 +10,51 @@ public class Library {
 
     // TODO: Add book to array
     public void addBook(Book book) {
-        // implement
+        if (count < books.length){
+            books[count]=book;
+            count++;}
+        else{
+            System.out.println("full");}
     }
 
     // TODO: Display all books
     public void displayBooks() {
-        // implement
-    }
+        for(int i = 0 ; i<count;i++){
+            System.out.println(books[i]);}}
+
 
     // TODO: Search book by title
     public Book searchByTitle(String title) {
+        for (int i = 0; i<count; i++){
+            if (books[i].getTitle().equals(title)){
+                return books[i];
+            }
+        }
         return null;
     }
 
     // TODO: Borrow book by title
     public void borrowBook(String title) {
-        // implement
+        Book b=searchByTitle(title);
+
+        if (b==null){
+            System.out.println("book not found");}
+        else if (!b.isAvailable()){
+            System.out.println("is already borrowed");}
+        else {
+            System.out.println("borrowed ");}
     }
 
     // TODO: Return book by title
     public void returnBook(String title) {
-        // implement
+        Book b=searchByTitle(title);
+        if (b == null) {
+            System.out.println("Book not found");
+        } else if (!b.isAvailable()) {
+            System.out.println("Book was not borrowed");
+        } else {
+            b.returnBook();
+            System.out.println("Book returned successfully");}
+
     }
 }
